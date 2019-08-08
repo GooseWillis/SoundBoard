9398c25e53c5f7f777998967e131ab8798ba2ede
GooseWillis <32306640+GooseWillis@users.noreply.github.com>
2019-08-08 08:48:26 -0500

    Delete SoundBoard.cmd

--- SoundBoard.cmd
+++
@@ -1,4 +0,0 @@
-REM PowerShell Jukebox by Jason Groce and improved by Tim Salmonson (renamed to SoundBoard)
-
-PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""%~dpn0.ps1""'}"
-PAUSE
